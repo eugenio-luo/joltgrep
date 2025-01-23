2e2c1ee13ee852210cbc38ea43be6578e1d40de5
Eugenio Luo <eugenio@Eugenios-MacBook-Air-8.local>
2025-01-23 18:35:12 +0000

    Added more testing features

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,3 +5,47 @@
 void tests::main(void)
 {
 }
+
+static std::size_t numTests = 0; 
+
+void tests::sectionStart(const std::string_view output)
+{
+    numTests = 0;
+    std::cout << "\033[1;33mSection Start: \033[0m" << output << "\n";
+}
+
+void tests::sectionEnd(const std::string_view output)
+{
+    std::cout << "\033[1;32mSection Success, " << numTests << " tests passed: \033[0m" << output << "\n";
+    numTests = 0;
+}
+
+void tests::start(const std::string_view output, int n)
+{
+    std::cout << "\033[1;33mTest Start: \033[0m" << output << " " << n << "\n";
+}
+void tests::start(const std::string_view output)
+{
+    std::cout << "\033[1;33mTest Start: \033[0m" << output << "\n";
+
+}
+
+void tests::end(const std::string_view output, int n)
+{
+    ++numTests;
+    std::cout << "\033[1;32mTest Success:\033[0m " << output << " " << n << "\n";
+}
+
+void tests::end(const std::string_view output)
+{
+    ++numTests;
+    std::cout << "\033[1;32mTest Success:\033[0m " << output << "\n";
+}
+
+void tests::check(bool condition, const std::string_view output)
+{
+    if (!condition) {
+        std::cout << "\033[1;31mTest Failed:\033[0m " << output << "\n";
+        exit(1);
+    }
+}
--- tests/tests.h
+++ tests/tests.h
@@ -1,6 +1,18 @@
 #pragma once
 
+#include <string>
+
 namespace tests {
+
+void sectionStart(const std::string_view output);
+void sectionEnd(const std::string_view output);
+
+void start(const std::string_view output);
+void start(const std::string_view output, int n);
+void end(const std::string_view output); 
+void end(const std::string_view output, int n); 
+
+void check(bool condition, const std::string_view output);
 
 void main(void);
 
