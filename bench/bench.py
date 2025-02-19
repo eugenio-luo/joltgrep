40ad2e72a4780fdad004e30e8118df2726e832f7
Eugenio Luo <eugenio@Eugenios-MacBook-Air-9.local>
2025-02-19 20:08:53 +0000

    improved bench.py to calculate mean and standard deviation

--- bench/bench.py
+++ bench/bench.py
@@ -2,11 +2,12 @@
 
 import time
 import subprocess
+import statistics
 
 WARMUP_SAMPLES = 3
 EXPERIMENT_SAMPLES = 20
 PATTERN = "queue" 
-PATH = "../bulbOS/src"
+PATH = "../linux/arch"
 
 def run(cmd):
     start = time.perf_counter_ns()
@@ -16,30 +17,41 @@ def run(cmd):
 
     return ret, end - start;
 
-def print_result(ret, time):
-    print("command: {0}, time: {1:.3f} ms".format(ret.args, time / 1000000))
-    if (ret.returncode != 0):
-        print(ret.stdout)
-        print("Errors:\n{0}".format(ret.stderr));
+def print_result(cmd, samples):
+    sample_mean = statistics.mean(samples)
+    sample_std = statistics.stdev(samples, sample_mean)
+    
+    print("command: {0}, time: {1:.3f} ms ± {2:.3f} ms".format(cmd, sample_mean / 1000000, sample_std / 1000000))
 
 def benchmark(cmd):
+    print(cmd)
     for i in range(0, WARMUP_SAMPLES):
+        print("warmup", i)
         run(cmd)
 
-    total_time = 0
+    samples = []
     for i in range(0, EXPERIMENT_SAMPLES):
+        print("run", i)
         ret, time = run(cmd)
-        total_time += time
+        if ret.returncode != 0:
+            print(ret.stdout)
+            print("Errors:\n{0}".format(ret.stderr));
+        samples.append(time)
 
-    print_result(ret, total_time / EXPERIMENT_SAMPLES)
+    return samples 
 
 def main():
     cmds = [["build/bin/joltgrep", PATTERN, PATH],
-            ["grep", "-r", PATTERN, PATH],
+            ["ggrep", "-r", PATTERN, PATH],
             ["rg", "-N", PATTERN, PATH]]
 
+    results = []
+
     for cmd in cmds:
-        benchmark(cmd)
+        results.append((cmd, benchmark(cmd)))
+
+    for cmd, samples in results:
+        print_result(cmd, samples)
 
 if __name__ == "__main__":
     main()
