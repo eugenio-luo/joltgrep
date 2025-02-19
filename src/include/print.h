715363a45766f8b6345eaa370d1401287678aa63
Eugenio Luo <eugenio@Eugenios-MacBook-Air-9.local>
2025-02-19 20:13:12 +0000

    implemented debugPrint

--- src/include/print.h
+++ src/include/print.h
@@ -7,5 +7,6 @@
 namespace joltgrep {
 
 void printLine(joltgrep::Task& task, std::string_view buffer, std::size_t pos);
+void debugPrint(std::string_view buffer);
 
 } // namespace joltgrep
--- src/print.cpp
+++ src/print.cpp
@@ -3,11 +3,11 @@
 
 #include "print.h"
 
+static std::mutex printLock{};
+
 void joltgrep::printLine(joltgrep::Task& task, 
     std::string_view buffer, std::size_t pos)
 {
-    static std::mutex printLock{};
-
     std::size_t left = pos, right = pos;
 
     while ((left > 0 && buffer[left] != '\n') || 
@@ -32,5 +32,12 @@ void joltgrep::printLine(joltgrep::Task& task,
     printLock.lock();
     std::cout << task.getPath() << ":" << 
         buffer.substr(left, right - left + 1) << "\n"; 
+    printLock.unlock();
+}
+
+void joltgrep::debugPrint(std::string_view buffer)
+{
+    printLock.lock();
+    std::cout << buffer;
     printLock.unlock();
 }
