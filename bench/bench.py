528b5efc2e64fdaa19bfe12ab46692110db1ca8c
Eugenio Luo <eugenio@Eugenios-MacBook-Air-9.local>
2025-02-22 01:03:57 +0000

    improved output

--- bench/bench.py
+++ bench/bench.py
@@ -5,9 +5,9 @@ import subprocess
 import statistics
 
 WARMUP_SAMPLES = 3
-EXPERIMENT_SAMPLES = 20
+EXPERIMENT_SAMPLES = 30
 PATTERN = "queue" 
-PATH = "../linux/arch"
+PATH = "../linux/drivers"
 
 def run(cmd):
     start = time.perf_counter_ns()
@@ -17,41 +17,63 @@ def run(cmd):
 
     return ret, end - start;
 
-def print_result(cmd, samples):
+def print_result(cmd, samples, ret):
     sample_mean = statistics.mean(samples)
     sample_std = statistics.stdev(samples, sample_mean)
     
-    print("command: {0}, time: {1:.3f} ms ± {2:.3f} ms".format(cmd, sample_mean / 1000000, sample_std / 1000000))
+    print("{0}{1}{2} => {3} matches, {4:.3f} ms ± {5:.3f} ms"
+          .format("'", " ".join(cmd), "'", ret.decode("ascii")[:-1], 
+                  sample_mean / 1000000, sample_std / 1000000))
 
 def benchmark(cmd):
-    print(cmd)
     for i in range(0, WARMUP_SAMPLES):
-        print("warmup", i)
+        print("'", end="")
+        print(" ".join(cmd), end="")
+        print("'", end="")
+        print(": ", end="")
+        print("warmup", i, end="\r")
         run(cmd)
+
+    print("\033[K", end="\r")
 
     samples = []
     for i in range(0, EXPERIMENT_SAMPLES):
-        print("run", i)
+        print("'", end="")
+        print(" ".join(cmd), end="")
+        print("'", end="")
+        print(": ", end="")
+        print("run", i, end="\r")
         ret, time = run(cmd)
         if ret.returncode != 0:
             print(ret.stdout)
             print("Errors:\n{0}".format(ret.stderr));
         samples.append(time)
 
-    return samples 
+    ret = subprocess.check_output(" ".join(cmd) + "| wc -l", shell=True)
+
+    print("'", end="")
+    print(" ".join(cmd), end="")
+    print("'", end="")
+    print(": \033[0;32mSuccess!\033[0;0m")
+    return samples, ret 
 
 def main():
-    cmds = [["build/bin/joltgrep", PATTERN, PATH],
+    cmds = [
             ["ggrep", "-r", PATTERN, PATH],
-            ["rg", "-N", PATTERN, PATH]]
+            ["build/bin/joltgrep", PATTERN, PATH],
+            ["rg", "-N", PATTERN, PATH]
+           ]
 
     results = []
 
     for cmd in cmds:
-        results.append((cmd, benchmark(cmd)))
+        samples, ret = benchmark(cmd)
+        results.append((cmd, samples, ret))
 
-    for cmd, samples in results:
-        print_result(cmd, samples)
+    print()
+    for cmd, samples, ret in results:
+        print_result(cmd, samples, ret)
+    print()
 
 if __name__ == "__main__":
     main()
