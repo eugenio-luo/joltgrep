68a5f6e310e219bfea63ce99b794ce3be2584eb5
Eugenio Luo <eugenio@Eugenios-MacBook-Air-9.local>
2025-03-02 18:56:25 +0000

    benchmark to assess correctness of output

---
+++ bench/correct.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+def check_output(cmds, arg):
+    for cmd in cmds:
+        cmd_str = " ".join(cmd) + " " + " ".join(arg)
+        ret = subprocess.check_output( cmd_str + "| wc -l", shell=True)
+        lines = ret.decode("ascii")[:-1]
+        print("'{0}' => {1} matches"
+          .format(cmd_str, ret.decode("ascii")[:-1])) 
+
+    print()
+
+def main():
+    cmds = [ ["build/bin/joltgrep"],
+             ["ggrep", "-r"] ] 
+    
+    args = [ ["queue", "../bulbOS/src"],
+             ["queue", "../linux/drivers"],   
+             ["PM_RESUME", "../linux/drivers"] ] 
+
+    for arg in args:
+        check_output(cmds, arg)
+
+if __name__ == "__main__":
+    main()
